b25d2cbec2b4a5fdedd676979bf2e1ffdc0ba13c
Eli Sacks <bastlifa@gmail.com>
2019-06-14 12:19:31 -0400

    MVP plus Stretch completed

--- challenges/classes.js
+++ challenges/classes.js
@@ -12,8 +12,6 @@ class CuboidMaker
     surfaceArea() { return 2 * (this.length * this.width  + this.length * this.height + this.width * this.height); }
 }
 
-
-
 const cuboid = new CuboidMaker
 ({
     length: 4,
